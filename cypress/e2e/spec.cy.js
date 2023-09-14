dc1589b93867c6f98e922cc82f22d18021cf5946
Giulia Verruck Tortola <giuliaverruck@gmail.com>
2023-09-14 19:53:59 -0300

    segunda

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,21 +1,21 @@
 describe('template spec', () => {
 
-  const name = "HAOISNDOIEMCD"
-  let email = "aaaaaa@jdpasjoda.com"
-  let password = "a123456a789s"
-  let lastName = "Tortolaaaa"
+  const name = "HAAASSSOISNDOIEMCD"
+  let email = "aaaaAaaaa@joda.com"
+  let password = "a12345SS6a789s"
+  let lastName = "TortolSSSaaaa"
   let company = "NTT DATA"
   let adress = "Rua da Flores, 245, Bairro Inexistente"
   let state = "SC"
   let city = "Cidade Invisível"
   let zipCode = '00000000'
-  let mobileNumber='+5588888888888'
+  let mobileNumber='+55888880988888'
   it('Validação primeira etapa cadastro', () => {
     cy.visit('https://automationexercise.com/')
     cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
     cy.get('[data-qa="signup-name"]').type(name);
     cy.get('[data-qa="signup-email"]').type(email);
-    cy.get('[data-qa="signup-button"').click()
+    cy.get('[data-qa="signup-button]"').click()
   })
 
   it.only('Validação cadastro completo', () => {
@@ -31,6 +31,14 @@ describe('template spec', () => {
     cy.get('[data-qa="years"]').select("1996")
     cy.get('[data-qa="first_name"]').type(name)
     cy.get('[data-qa="last_name"]').type(lastName)
-
+    cy.get('#company').type(company)
+    cy.get('#address1').type(adress)
+    cy.get('#country').select("Canada")
+    cy.get('[data-qa="state"]').type(state)
+    cy.get('#city').type(city)
+    cy.get('#zipcode').type(zipCode)
+    cy.get('#mobile_number').type(mobileNumber)
+    cy.get('[data-qa="create-account"]').click()
+   // cy.get('b').should('have.value',"ACCOUNT CREATED!")
   })
 })
